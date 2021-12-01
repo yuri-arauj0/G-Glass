b05187cf9d43152f00736c64db4d6eae45f17ce4
Yuri A. Alves <yuri.a2alves@gmail.com>
2021-11-30 23:22:33 -0300

    Delete funcoes.js

--- funcoes.js
+++
@@ -1,9 +0,0 @@
-function changePic(pic) {
-    document.getElementById('icone').src = pic;
-}
-
-function _calcTotal() {
-    var qtd = parseInt(document.getElementById('f-qtd').value);
-    total = qtd * 1500;
-    document.getElementById('f-total').value = total; 
-}
